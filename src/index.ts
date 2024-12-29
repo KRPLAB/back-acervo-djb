a19ae17366c88e78ac5520935c3c26a6eb83e24f
krplab <kauan.r.paulino@gmail.com>
2024-12-28 23:54:04 -0300

    pasta uploads disponibilizada para servir arquivos estáticos

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servindo arquivos estáticos da pasta "uploads"
+app.use(express.static("uploads"));
+
 app.get("/", (req, res) => {
     res.send("Olá, server rodando");
 });
