73b3eef55ac823bc2af1e62fd52bd35be186156b
krplab <kauan.r.paulino@gmail.com>
2025-01-08 20:52:27 -0300

    interface IBook criada

---
+++ src/types/bookInterface.ts
@@ -0,0 +1,14 @@
+export interface Ibook {
+    Registro: string;
+    data: string;
+    autor: string;
+    titulo: string;
+    editora: string;
+    ano: string;
+    exemplares: string;
+    qtdEmprestado: string;
+    qtdDisponivel: string;
+    generoLiterario: string;
+    capa: string;
+    emprestadoPara: string[];
+}
